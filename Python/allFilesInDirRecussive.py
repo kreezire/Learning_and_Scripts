fddf6a983c9b20e066f1707a0db77a891a48a3e6
Vipul Aggarwal <vipaggar@adobe.com>
2017-07-04 14:52:48 +0530

    running shell script on each file

--- Python/allFilesInDirRecussive.py
+++ Python/allFilesInDirRecussive.py
@@ -2,9 +2,13 @@ import os
 from glob import glob
 
 files = []
-start_dir = "Z:\\users\\vipaggar\\124917_Callgrind"#os.getcwd()
+start_dir = "/devdisk/users/vipaggar/output/callgrindData/"#os.getcwd()
 pattern   = "*.out"#"*.log"
 
 for dir,_,_ in os.walk(start_dir):
     files.extend(glob(os.path.join(dir,pattern))) 
 #sprint files
+
+for file in files:
+	print "callgrind_annotate --tree=caller " + file + " &> /devdisk/users/vipaggar/output/callgrindData/annotated/"+os.path.basename(file)
+	os.system("callgrind_annotate --tree=caller " + file + " &> /devdisk/users/vipaggar/output/callgrindData/annotated/"+os.path.basename(file))
